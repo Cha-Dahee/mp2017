e07894521b132ed488d0747ccc384731a5857457
Joy\Dahee Joy Cha <ekgmlekgml20@naver.com>
2017-11-05 14:13:36 +0900

    'beforeGeo'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,40 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { EditMeetingInfoPage} from '../pages/edit-meeting-info/edit-meeting-info';
+import { EditMeetingRulePage} from '../pages/edit-meeting-rule/edit-meeting-rule';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaK_t5ngY0MZlIQW42ygjCCGDFJL--Er0",
+  authDomain: "meetinghandler.firebaseapp.com",
+  databaseURL: "https://meetinghandler.firebaseio.com",
+  projectId: "meetinghandler",
+  storageBucket: "meetinghandler.appspot.com",
+  messagingSenderId: "580575550249"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EditMeetingInfoPage,
+    EditMeetingRulePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EditMeetingInfoPage,
+    EditMeetingRulePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {FirebaseListObservable, AngularFireDatabase} from 'angularfire2/database';
+
+import {Member} from './member';
+import { EditMeetingInfoPage} from '../edit-meeting-info/edit-meeting-info';
+import { EditMeetingRulePage} from '../edit-meeting-rule/edit-meeting-rule';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  members:FirebaseListObservable<any[]>;
 
+  constructor(public navCtrl: NavController, public af:AngularFireDatabase) {
+    this.members = af.list('/member');
   }
 
+  //DB 읽기용 mock function
+  addMember(){
+      this.members.push({name:'하은',id:'Haeun', password:'1234'});
+      this.members.push({name:'다희',id:'Dahee', password:'1234'});
+      this.members.push({name:'민우',id:'Minwoo', password:'1234'});
+      this.members.push({name:'소정',id:'Sojung', password:'1234'});
+    
+  }
+
+  removeMember(member:Member){
+    let toBeDeleted:string = prompt("누굴 삭제할까요");
+    if(toBeDeleted !=''){
+      this.members.remove(toBeDeleted);
+    }
+  }
+
+  goEditMeetingInfoPage(){
+    this.navCtrl.push(EditMeetingInfoPage);
+  }
+
+  goEditMeetingRulePage(){
+    this.navCtrl.push(EditMeetingRulePage);
+  }
+  
 }
